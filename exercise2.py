463befb47b3d86bbf7d0ad881088568aa0215a2a
Tue Jensen <tuebjensen@gmail.com>
2025-05-27 18:44:57 +0200

    update plots for exercise 2

--- exercise2.py
+++ exercise2.py
@@ -10,21 +10,19 @@ class BNN(nn.Module):
     def __init__(self, input_dim, output_dim, hidden_dim=64):
         super().__init__()
         self.fc1 = nn.Linear(input_dim, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
         self.fc_mean = nn.Linear(hidden_dim, output_dim)
         self.fc_log_var = nn.Linear(hidden_dim, output_dim)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
         mean = self.fc_mean(x)
         log_var = self.fc_log_var(x)
-        return mean, log_var
+        return mean, torch.exp(log_var)
 
 
-def generate_training_data(device, num_samples=100):
+def generate_data(device="cpu", lower=0.0, upper=1.0, num_samples=100):
     # Generate synthetic training data with a sine wave and gaussian noise with variance of 0.1
-    x = np.linspace(0, 1, num_samples)
+    x = np.linspace(lower, upper, num_samples)
     sine_wave = np.sin(2 * np.pi * x)
     noised_sine_wave = sine_wave + np.random.normal(0, 0.1, num_samples)
     x = torch.tensor(x, dtype=torch.float32).unsqueeze(-1).to(device)
@@ -35,22 +33,11 @@ def generate_training_data(device, num_samples=100):
 
 
 def main():
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = "cuda" if torch.cuda.is_available() else "cpu"
     # Generate training data
-    x_train, y_train, y_clean = generate_training_data(device)
+    x_train, y_train, y_clean = generate_data(device)
 
-    # Plot the training data
-    # plt.figure(figsize=(10, 5))
-    # plt.plot(x.cpu().numpy(), y.cpu().numpy(), "o", label="Training Data")
-    # plt.title("Training Data")
-    # plt.xlabel("Input")
-    # plt.ylabel("Output")
-    # plt.legend()
-    # plt.grid()
-    # plt.savefig("plots/exercise2_training_data.png", bbox_inches="tight")
-    # plt.show()
-
-    # Define the Bayesian Neural Network
+    # Define the Bayesian Neural Network an optimizer
     input_dim = 1
     output_dim = 1
     model = BNN(input_dim, output_dim).to(device)
@@ -59,16 +46,15 @@ def main():
     num_epochs = 1000
     losses = []
     for epoch in range(num_epochs):
-        # Forward pass
-        mean, log_var = model(x_train)
-        loss = criterion(mean, y_train, torch.exp(log_var))
-
-        # Backward pass and optimization
+        model.train()
         optimizer.zero_grad()
+
+        mean_pred, var_pred = model(x_train)
+        loss = criterion(mean_pred, y_train, var_pred)
         loss.backward()
         optimizer.step()
-        losses.append(loss.item())
 
+        losses.append(loss.item())
         if epoch % 100 == 0:
             print(f"Epoch [{epoch}/{num_epochs}], Loss: {loss.item():.4f}")
 
@@ -82,29 +68,30 @@ def main():
     plt.grid()
     plt.savefig("plots/exercise2_training_loss.png", bbox_inches="tight")
 
+    x_test, _, _ = generate_data(device, num_samples=1000)
     model.eval()
     with torch.no_grad():
-        mean, log_var = model(x_train)
+        mean_pred, var_pred = model(x_test)
 
-    mean = mean.squeeze().cpu().numpy()
-    log_var = log_var.squeeze().cpu().numpy()
+    mean_pred = mean_pred.squeeze().cpu().numpy()
+    var_pred = var_pred.squeeze().cpu().numpy()
     x_train = x_train.squeeze().cpu().numpy()
     y_train = y_train.squeeze().cpu().numpy()
-    var_pred = np.exp(log_var)
+    x_test = x_test.squeeze().cpu().numpy()
     std_pred = np.sqrt(var_pred)
-    lower_bound = mean - 2 * std_pred
-    upper_bound = mean + 2 * std_pred
+    lower_bound = mean_pred - 2 * std_pred
+    upper_bound = mean_pred + 2 * std_pred
 
     # Plot the predictions with uncertainty bounds
     plt.figure(figsize=(10, 5))
     plt.plot(
-        x_train,
-        mean,
+        x_test,
+        mean_pred,
         label="Predicted Mean",
         color="orange",
     )
     plt.fill_between(
-        x_train,
+        x_test,
         lower_bound,
         upper_bound,
         color="lightblue",
@@ -118,7 +105,45 @@ def main():
     plt.ylabel("Output")
     plt.legend()
     plt.grid()
-    plt.savefig("plots/exercise2_predictions_with_uncertainty.png", bbox_inches="tight")
+    plt.savefig("plots/exercise2_predictions.png", bbox_inches="tight")
+
+    x_test, _, y_test_clean = generate_data(
+        device, lower=-0.5, upper=1.5, num_samples=1000
+    )
+    with torch.no_grad():
+        mean_pred, var_pred = model(x_test)
+
+    mean_pred = mean_pred.squeeze().cpu().numpy()
+    var_pred = var_pred.squeeze().cpu().numpy()
+    x_test = x_test.squeeze().cpu().numpy()
+    std_pred = np.sqrt(var_pred)
+    lower_bound = mean_pred - 2 * std_pred
+    upper_bound = mean_pred + 2 * std_pred
+
+    # Plot the predictions with uncertainty bounds
+    plt.figure(figsize=(10, 5))
+    plt.plot(
+        x_test,
+        mean_pred,
+        label="Predicted Mean",
+        color="orange",
+    )
+    plt.fill_between(
+        x_test,
+        lower_bound,
+        upper_bound,
+        color="lightblue",
+        alpha=0.5,
+        label="Uncertainty Bounds (2 std)",
+    )
+    plt.plot(x_test, y_test_clean, label="True Function", color="green")
+    plt.plot(x_train, y_train, "o", label="Training Data")
+    plt.title("Bayesian Neural Network Predictions with Uncertainty")
+    plt.xlabel("Input")
+    plt.ylabel("Output")
+    plt.legend()
+    plt.grid()
+    plt.savefig("plots/exercise2_test_predictions.png", bbox_inches="tight")
 
 
 if __name__ == "__main__":
